498965049b2f339a78d203e9f9a47068f4c5fb88
Rajib Sarker <redhat0593@gmail.com>
2023-01-04 02:14:03 +0600

    booking index created

--- src/Pages/HomePage/HomeBanner/HomeBanner.js
+++ src/Pages/HomePage/HomeBanner/HomeBanner.js
@@ -5,6 +5,10 @@ import '../HomeBanner/HomeBanner.css';
 
 const HomeBanner = () => {
     const [quantityDialogue, setQuantityDialogue] = useState(false);
+    const [checkIn, setCheckIn] = useState(false);
+    const [indate, setIndate] = useState("Check In");
+    const [outdate, setOutdate] = useState("Check Out");
+    const [checkOut, setCheckOut] = useState(false);
     const [room, setRooms] = useState(2);
     const [adults, setAdults] = useState(3);
     const [child, setChilds] = useState(0);
@@ -16,84 +20,128 @@ const HomeBanner = () => {
             <div className='d-flex align-items-center'>
                 <div className='buttons-wrapper me-2'>
                     <div className='d-flex align-items-center justify-content-center position-relative'>
-                        <button className='search-filter-button'>
-                            <FaCalendarAlt />
-                            <span className='mx-2'>Check In</span>
-                            <FaChevronDown />
-                        </button>
-                        <button className='search-filter-button'>
-                            <FaCalendarAlt />
-                            <span className='mx-2'>Check Out</span>
-                            <FaChevronDown />
-                        </button>
-                        <button
-                            onClick={() => setQuantityDialogue(!quantityDialogue)}
-                            className='search-filter-button'>
-                            <p className='mb-0'>{adults} Adults. {child} Childres. {room} Rooms <FaChevronDown /></p>
-                        </button>
+                        <div className='search-filter-button'>
+                            <button
+                                className="buton"
+                                onClick={() => setCheckIn(!checkIn)}
+                            >
+                                <FaCalendarAlt />
+                                <span className='mx-2'>{indate}</span>
+                                <FaChevronDown />
+                            </button>
 
-                        {
-                            quantityDialogue &&
-                            <div className='quantity'>
-                                <p>Lorem ipsum dolor sit amet.</p>
-                                <div className='d-flex mb-2 justify-content-between'>
-                                    <div>
-                                        <h6 className='m-0'>Rooms</h6>
-                                        <p>Lorem ipsum dolor sit </p>
-                                    </div>
-                                    <div className='d-flex align-items-center'>
-                                        <button
-                                            onClick={() => setRooms(room - 1)}
-                                            className='calc-button'
-                                            disabled={room === 1}
-                                        >-</button>
-                                        <span className='fs-5 mx-1'> {room} </span>
-                                        <button
-                                            onClick={() => setRooms(room + 1)}
-                                            className='calc-button'>+</button>
-                                    </div>
-                                </div>
-                                <div className='d-flex mb-2 justify-content-between'>
-                                    <div>
-                                        <h6 className='m-0'>Adults</h6>
-                                        <p>Lorem ipsum dolor sit </p>
-                                    </div>
-                                    <div className='d-flex align-items-center'>
-                                        <button
-                                            onClick={() => setAdults(adults - 1)}
-                                            className='calc-button'
-                                            disabled={adults === 1}
-                                        >-</button>
-                                        <span className='fs-5 mx-1'> {adults} </span>
-                                        <button
-                                            onClick={() => setAdults(adults + 1)}
-                                            className='calc-button'>+</button>
-                                    </div>
-                                </div>
-                                <div className='d-flex mb-2 justify-content-between'>
-                                    <div>
-                                        <h6 className='m-0'>Children</h6>
-                                        <p>Lorem ipsum dolor sit </p>
-                                    </div>
-                                    <div className='d-flex align-items-center'>
-                                        <button
-                                            onClick={() => setChilds(child - 1)}
-                                            className='calc-button'
-                                            disabled={child === 0}
-                                        >-</button>
-                                        <span className='fs-5 mx-1'> {child} </span>
-                                        <button
-                                            onClick={() => setChilds(child + 1)}
-                                            className='calc-button'>+</button>
-                                    </div>
-                                </div>
-                                <div className="text-center">
+                            {checkIn &&
+                                <div className='date-box'>
+                                    <input
+                                        onChange={(e) => {
+                                            setIndate(e.target.value)
+                                        }}
+                                        type="date" className='form-control' />
+                                    <br />
                                     <button
-                                        onClick={() => setQuantityDialogue(false)}
+                                        onClick={() => setCheckIn(!checkIn)}
                                         className='button-done'>Done</button>
                                 </div>
-                            </div>
-                        }
+                            }
+                        </div>
+                        <div className='search-filter-button'>
+                            <button
+                                className="buton"
+                                onClick={() => setCheckOut(!checkOut)}>
+                                <FaCalendarAlt />
+                                <span className='mx-2'>{outdate}</span>
+                                <FaChevronDown />
+                            </button>
+
+                            {checkOut &&
+                                <div className='date-box'>
+                                    <input
+                                        onChange={(e) => {
+                                            setOutdate(e.target.value)
+                                        }}
+                                        type="date" className='form-control' />
+                                    <br />
+                                    <button
+                                        onClick={() => setCheckOut(!checkOut)}
+                                        className='button-done'>Done</button>
+                                </div>
+                            }
+
+                        </div>
+                        <div
+                            className='search-filter-button border-0'
+                        >
+                            <button
+                                className="buton"
+                                onClick={() => setQuantityDialogue(!quantityDialogue)}>
+                                <p className='mb-0'>{adults} Adults. {child} Childres. {room} Rooms <FaChevronDown /></p>
+                            </button>
+
+
+                            {
+                                quantityDialogue &&
+                                <div className='quantity'>
+                                    <p>Lorem ipsum dolor sit amet.</p>
+                                    <div className='d-flex mb-2 justify-content-between'>
+                                        <div>
+                                            <h6 className='m-0'>Rooms</h6>
+                                            <p>Lorem ipsum dolor sit </p>
+                                        </div>
+                                        <div className='d-flex align-items-center'>
+                                            <button
+                                                onClick={() => setRooms(room - 1)}
+                                                className='calc-button'
+                                                disabled={room === 1}
+                                            >-</button>
+                                            <span className='fs-5 mx-1'> {room} </span>
+                                            <button
+                                                onClick={() => setRooms(room + 1)}
+                                                className='calc-button'>+</button>
+                                        </div>
+                                    </div>
+                                    <div className='d-flex mb-2 justify-content-between'>
+                                        <div>
+                                            <h6 className='m-0'>Adults</h6>
+                                            <p>Lorem ipsum dolor sit </p>
+                                        </div>
+                                        <div className='d-flex align-items-center'>
+                                            <button
+                                                onClick={() => setAdults(adults - 1)}
+                                                className='calc-button'
+                                                disabled={adults === 1}
+                                            >-</button>
+                                            <span className='fs-5 mx-1'> {adults} </span>
+                                            <button
+                                                onClick={() => setAdults(adults + 1)}
+                                                className='calc-button'>+</button>
+                                        </div>
+                                    </div>
+                                    <div className='d-flex mb-2 justify-content-between'>
+                                        <div>
+                                            <h6 className='m-0'>Children</h6>
+                                            <p>Lorem ipsum dolor sit </p>
+                                        </div>
+                                        <div className='d-flex align-items-center'>
+                                            <button
+                                                onClick={() => setChilds(child - 1)}
+                                                className='calc-button'
+                                                disabled={child === 0}
+                                            >-</button>
+                                            <span className='fs-5 mx-1'> {child} </span>
+                                            <button
+                                                onClick={() => setChilds(child + 1)}
+                                                className='calc-button'>+</button>
+                                        </div>
+                                    </div>
+                                    <div className="text-center">
+                                        <button
+                                            onClick={() => setQuantityDialogue(false)}
+                                            className='button-done'>Done</button>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+
                     </div>
                 </div>
                 <div>
